b7fd4763dfacc05c98e3223b60e63e2dae74eff5
Michael Goldman <goldmandrummer@gmail.com>
2014-10-04 05:20:38 -0500

    Merge

--- server/twilio.js
+++ server/twilio.js
@@ -1,3 +1,13 @@
-sendText = function (text, callback) {
+var sendText = function (text, callback) {
 
+	twilioClient = Twilio('AC684d54cb44bd455d3ec5e1a49e3f6d09', '53d4184fba04016a5450abac88340323');
+	twilioClient.sendSms({
+		to:   '+15555555555',
+		from: '+15555555555',
+		body: text
+	}, callback);  // callback(err, data)
+};
+
+onResponse = function (argument) {
+	// body...
 };
