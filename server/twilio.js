fe7fb688f77e01bdd3fbecae51c7d726ecb92389
Michael Goldman <goldmandrummer@gmail.com>
2014-10-04 11:35:47 -0500

    Fixed some layout sizing and added test of Twilio

--- server/twilio.js
+++ server/twilio.js
@@ -1,13 +1,21 @@
-var sendText = function (text, callback) {
+var sendText = function (text) {
 
 	twilioClient = Twilio('AC684d54cb44bd455d3ec5e1a49e3f6d09', '53d4184fba04016a5450abac88340323');
 	twilioClient.sendSms({
-		to:   '+15555555555',
-		from: '+15555555555',
+		to:   '+18152368052',
+		from: '+18155069261',
 		body: text
-	}, callback);  // callback(err, data)
-};
-
-onResponse = function (argument) {
-	// body...
+	}, function (err, response) {
+		if (!err) {
+			if (response.trim().toLowerCase().equals(buy)) {
+				// buy given stock
+			}
+			if (response.trim().toLowerCase().equals(sell)) {
+				// buy given stock
+			}
+		}
+		else {
+			console.log(err.message)
+		}
+	});
 };
