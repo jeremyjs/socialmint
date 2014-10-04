d267ca7110e6687129fc08eb69a31bbbd4631aa1
Jeremy Meyer <jjman505@gmail.com>
2014-10-04 16:38:28 -0400

    Yo

---
+++ client/js/config.js
@@ -0,0 +1,8 @@
+
+Highcharts.theme = {
+    colors: ['#50B432', '#C53030', '#058DC7', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+             '#FF9655', '#FFF263', '#6AF9C4']
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
---
+++ client/js/plugins/chart.js
@@ -0,0 +1,236 @@
+/**
+ * Version 2.0
+ */
+
+Markit = {};
+
+/**
+ * Define the InteractiveChartApi.
+ * First argument is symbol (string) for the quote. Examples: AAPL, MSFT, JNJ, GOOG.
+ * Second argument is duration (int) for how many days of history to retrieve.
+ */
+Markit.InteractiveChartApi = function(symbol,duration){
+    this.symbol = symbol.toUpperCase();
+    this.duration = duration;
+    this.PlotChart();
+};
+
+Markit.InteractiveChartApi.prototype.PlotChart = function(){
+
+    var params = {
+        parameters: JSON.stringify( this.getInputParams() )
+    }
+
+    //Make JSON request for timeseries data
+    $.ajax({
+        beforeSend:function(){
+            $("#chartDemoContainer").text("Loading chart...");
+        },
+        data: params,
+        url: "http://dev.markitondemand.com/Api/v2/InteractiveChart/jsonp",
+        dataType: "jsonp",
+        context: this,
+        success: function(json){
+            //Catch errors
+            if (!json || json.Message){
+                console.error("Error: ", json.Message);
+                return;
+            }
+            this.render(json);
+        },
+        error: function(response,txtStatus){
+            console.log(response,txtStatus)
+        }
+    });
+};
+
+Markit.InteractiveChartApi.prototype.getInputParams = function(){
+    return {
+        Normalized: false,
+        NumberOfDays: this.duration,
+        DataPeriod: "Day",
+        Elements: [
+            {
+                Symbol: this.symbol,
+                Type: "price",
+                Params: ["ohlc"] //ohlc, c = close only
+            },
+            {
+                Symbol: this.symbol,
+                Type: "volume"
+            }
+        ]
+        //,LabelPeriod: 'Week',
+        //LabelInterval: 1
+    }
+};
+
+Markit.InteractiveChartApi.prototype._fixDate = function(dateIn) {
+    var dat = new Date(dateIn);
+    return Date.UTC(dat.getFullYear(), dat.getMonth(), dat.getDate());
+};
+
+Markit.InteractiveChartApi.prototype._getOHLC = function(json) {
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[0]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = this._fixDate( dates[i] );
+            var pointData = [
+                dat,
+                elements[0].DataSeries['open'].values[i],
+                elements[0].DataSeries['high'].values[i],
+                elements[0].DataSeries['low'].values[i],
+                elements[0].DataSeries['close'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+    return chartSeries;
+};
+
+Markit.InteractiveChartApi.prototype._getVolume = function(json) {
+    var dates = json.Dates || [];
+    var elements = json.Elements || [];
+    var chartSeries = [];
+
+    if (elements[1]){
+
+        for (var i = 0, datLen = dates.length; i < datLen; i++) {
+            var dat = this._fixDate( dates[i] );
+            var pointData = [
+                dat,
+                elements[1].DataSeries['volume'].values[i]
+            ];
+            chartSeries.push( pointData );
+        };
+    }
+    return chartSeries;
+};
+
+Markit.InteractiveChartApi.prototype.render = function(data) {
+    //console.log(data)
+    // split the data set into ohlc and volume
+    var ohlc = this._getOHLC(data),
+        volume = this._getVolume(data);
+
+    // set the allowed units for data grouping
+    var groupingUnits = [[
+        'week',                         // unit name
+        [1]                             // allowed multiples
+    ], [
+        'month',
+        [1, 2, 3, 4, 6]
+    ]];
+
+    // create the chart
+    $('#chartDemoContainer').highcharts('StockChart', {
+
+        rangeSelector: {
+            selected: 1
+            //enabled: false
+        },
+
+        title: {
+            text: this.symbol + ' Historical Price'
+        },
+
+        yAxis: [{
+            title: {
+                text: 'Closing Price'
+            },
+            height: 200,
+            lineWidth: 2
+        }
+		, {
+            title: {
+                text: 'Volume'
+            },
+            top: 300,
+            height: 100,
+            offset: 0,
+            lineWidth: 2
+        }],
+
+        series: [{
+            //type: 'candlestick',
+            name: this.symbol,
+            data: ohlc,
+			regression: true ,
+                regressionSettings: {
+                    type: 'loess',
+                    color:  '#1111cc',
+                    loessSmooth: 50
+
+                },
+			shadow : true,
+			marker : {
+                    enabled : true,
+                    radius : 3
+                },
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }, {
+            type: 'column',
+            name: 'Volume',
+            data: volume,
+            yAxis: 1,
+            dataGrouping: {
+                units: groupingUnits
+            }
+        }],
+        credits: {
+            enabled:false
+        }
+    });
+};
+
+
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: 'Community<br>Sentiment',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 50
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white',
+                        textShadow: '0px 1px 2px black'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%']
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Sentiment',
+            innerSize: '50%',
+            data: [
+                ['Bullish', 25],
+                ['Bearish', 25],
+            ]
+        }]
+    });
+});
+
