b8035c7fe276f1a2a0f4f902a9a6e87bc1aa78d8
Jeremy Meyer <jjman505@gmail.com>
2014-10-05 10:24:52 -0400

    merge

--- collections/collections.js
+++ collections/collections.js
@@ -1,41 +1,5 @@
 Stocks = new Mongo.Collection('stocks');
-<<<<<<< HEAD
 
-var notifier = function (id, stock) {
-	
-	console.log(stock);
-	
-	var alerts = stock.Alerts;
-	for (var key in alerts) {
-		var alert = alerts[key];
-		if (stock.LastPrice < alert[low]) {
-
-			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice 
-							+ ", which is below your alert of $" + alert[low];
-		} else if (stock.LastPrice == alert[low]) {
-
-			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice 
-							+ ", which is exactly your alert of $" + alert[low];
-		} else if (stock.LastPrice > alert[high]) {
-
-			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice 
-							+ ", which is above your alert of $" + alert[high];
-		} else if (stock.LastPrice == alert[high]) {
-
-			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice 
-							+ ", which is exactly your alert of $" + alert[high];
-		} else {	continue;	};
-		
-		Meteor.call('sendText', message);
-	}
-}
-
-Stocks.find().observeChanges({
-
-	added: notifier,
-	changed: notifier
-});
-=======
 Perceptions = new Mongo.Collection('Perceptions');
 /* perception = {
  *   symbol: string,
@@ -47,4 +11,38 @@ Perceptions = new Mongo.Collection('Perceptions');
  *   }
  * }
  */
->>>>>>> FETCH_HEAD
+
+var notifier = function (id, stock) {
+
+	console.log(stock);
+
+	var alerts = stock.Alerts;
+	for (var key in alerts) {
+		var alert = alerts[key];
+		if (stock.LastPrice < alert[low]) {
+
+			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice
+							+ ", which is below your alert of $" + alert[low];
+		} else if (stock.LastPrice == alert[low]) {
+
+			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice
+							+ ", which is exactly your alert of $" + alert[low];
+		} else if (stock.LastPrice > alert[high]) {
+
+			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice
+							+ ", which is above your alert of $" + alert[high];
+		} else if (stock.LastPrice == alert[high]) {
+
+			var message = stock.Name + "  (" + stock.Symbol + ")  " + "has just dropped to $" + stock.LastPrice
+							+ ", which is exactly your alert of $" + alert[high];
+		} else {	continue;	};
+
+		Meteor.call('sendText', message);
+	}
+}
+
+Stocks.find().observeChanges({
+
+	added: notifier,
+	changed: notifier
+});
