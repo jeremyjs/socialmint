bf8c8238c78d9b520ea89c557fb6ff842857077a
Jeremy Meyer <jjman505@gmail.com>
2014-10-05 03:38:39 -0400

    text update function update

--- collections/collections.js
+++ collections/collections.js
@@ -1,1 +1,12 @@
 Stocks = new Mongo.Collection('stocks');
+Perceptions = new Mongo.Collection('Perceptions');
+/* perception = {
+ *   symbol: string,
+ *   sentiments: {
+ *     userId: {
+ *       bull: float,
+ *       bear: null,
+ *     }, ...
+ *   }
+ * }
+ */
