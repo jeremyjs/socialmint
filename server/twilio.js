60fcb8f9d1eb1fa5d6907a1d8659a9986f3198c0
Michael Goldman <goldmandrummer@gmail.com>
2014-10-04 23:10:27 -0500

    Twilio test worked. Dat shit cray

--- server/twilio.js
+++ server/twilio.js
@@ -1,11 +1,13 @@
 Meteor.methods({
 	sendText: function (text) {
-		twilioClient = Twilio('AC684d54cb44bd455d3ec5e1a49e3f6d09', '53d4184fba04016a5450abac88340323');
+
+		twilioClient = Meteor.npmRequire('twilio')('AC684d54cb44bd455d3ec5e1a49e3f6d09', '53d4184fba04016a5450abac88340323');
+		console.log(text);
 		twilioClient.sendSms({
 			to:   '+18152368052',
 			from: '+18155069261',
 			body: text
-		}, function (err, response) {
+		}, function (err, res) {
 			if (!err) {
 				if (response.trim().toLowerCase().equals(buy)) {
 					// buy given stock
