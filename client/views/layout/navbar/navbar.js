a231b0f57b0bf08e5072407f8dbd5e602f624b82
Jeremy Meyer <jjman505@gmail.com>
2015-07-10 23:10:31 -0400

    use the tickerSearch component

--- client/views/layout/navbar/navbar.js
+++ client/views/layout/navbar/navbar.js
@@ -8,11 +8,5 @@ Template.navbar.helpers({
 Template.navbar.events({
   'click .logout': function () {
     Meteor.logout();
-  },
-
-  'submit form.navbar-search': function (e) {
-    e.preventDefault();
-    var symbol = $('#navbar-search-box').val();
-    Router.go('/stock/show/' + symbol);
   }
 });
