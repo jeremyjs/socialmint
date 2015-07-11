8561555201fce895e5196a471e80e2ae9516444d
Jeremy Meyer <jjman505@gmail.com>
2015-07-10 22:48:41 -0400

    fixed up search input

--- client/views/layout/navbar/navbar.js
+++ client/views/layout/navbar/navbar.js
@@ -8,5 +8,11 @@ Template.navbar.helpers({
 Template.navbar.events({
   'click .logout': function () {
     Meteor.logout();
+  },
+
+  'submit form.navbar-search': function (e) {
+    e.preventDefault();
+    var symbol = $('#navbar-search-box').val();
+    Router.go('/stock/show/' + symbol);
   }
 });
